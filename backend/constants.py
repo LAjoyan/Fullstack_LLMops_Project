4bd5f03277943216b671a75e16153e20801940b3
Josefin Lesley <josefin3647@gmail.com>
2026-04-30 12:49:52 +0200

    Removed mlflow and added backend and frontend to projects pyproject.toml

--- backend/constants.py
+++ backend/constants.py
@@ -5,5 +5,5 @@ DATA_PATH = ROOT_PATH / "data"
 
 VECTOR_DB_PATH = ROOT_PATH / "knowledge_base"
 
-MODEL="openrouter:openai/gpt-4.1-nano"
+MODEL = "openrouter:openai/gpt-4.1-nano"
 EMBEDDING_MODEL = "embed-multilingual-light-v3.0"
