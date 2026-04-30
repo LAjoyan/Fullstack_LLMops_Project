640489feb191dffe3cdd152074e67c649f0e582b
erfantk25-lab <erfantk.25@gmail.com>
2026-04-30 22:20:11 +0200

    Refactor API by removing middleware and updating response
    Removed logging middleware and updated query response.

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-import constants
 from data_models import Prompt, RagResponse
-from agents import bot_answer
-from middleware import logging_middleware
 
 app = FastAPI()
 
-logging_middleware(app=app)
 
 @app.get("/")
 async def status():
@@ -14,5 +10,5 @@ async def status():
 
 @app.post("/rag/query")
 async def query_documentation(query: Prompt) -> RagResponse:
-    result = await bot_answer(query.prompt)
-    return resultv
+  
+    return RagResponse(answer="This is a placeholder answer until agent.py is created.")
