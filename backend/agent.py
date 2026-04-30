c6e5c8c630e55f96afc78a959518a2666712038f
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-04-30 13:10:52 +0200

    add ingestion pipeline for LanceDB

--- backend/agent.py
+++ backend/agent.py
@@ -2,6 +2,10 @@ from pydantic_ai import Agent
 from pydantic_ai.usage import UsageLimits
 import lancedb
 from backend.constants import MODEL
+from dotenv import load_dotenv
+import asyncio
+
+load_dotenv()
 
 vector_db = lancedb.connect(uri="./vector_db")
 
@@ -12,7 +16,18 @@ rag_agent = Agent(
 
 @rag_agent.tool_plain 
 def retrieve_documents(query: str, k: int=3) -> str:
-    results = vector_db["articles"].search(query=query).limit(k).to_list()
+    results = vector_db["LectureTranscript"].search(query=query).limit(k).to_list()
 
     if not results:
         return "no documents found."
+    return "\n\n".join([r["content"] for r in results])  
+
+async def main():
+    result = await rag_agent.run("Summarize the documents")
+    print(result.output)
+
+asyncio.run(main())
+
+print(vector_db.table_names())
+
+# Adding mlflow later
