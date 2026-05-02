1c0f6450d0263353713ffda0a0349d3e137b191a
Erfan Tahmasbikondroodi <erfantk.25@gmail.com>
2026-05-02 14:10:54 +0200

    Fixed agent tools, updated ingestion path, and got the full RAG pipeline working

--- backend/agent.py
+++ backend/agent.py
@@ -1,18 +1,42 @@
-from pydantic_ai import Agent
-from pydantic_ai.usage import UsageLimits
-import lancedb
-from backend.constants import MODEL
 
-vector_db = lancedb.connect(uri="./vector_db")
+from dotenv import load_dotenv
+load_dotenv() 
+from pydantic_ai import Agent
+import lancedb
+from backend.constants import MODEL, VECTOR_DB_PATH
+from backend.data_models import RagResponse
+
+vector_db = lancedb.connect(uri=VECTOR_DB_PATH)
 
 rag_agent = Agent(
-    model = MODEL,
-    system_prompt = "You are a expert reader and doing quizes and summarize youtube subtitle files"
+    model=MODEL,
+    system_prompt="You are an expert reader. Answer questions and summarize based on the retrieved documents.",
+    output_type=RagResponse, 
 )
 
 @rag_agent.tool_plain 
 def retrieve_documents(query: str, k: int=3) -> str:
-    results = vector_db["articles"].search(query=query).limit(k).to_list()
+    results = vector_db["LectureTranscript"].search(query=query).limit(k).to_list()
 
     if not results:
-        return "no documents found."
+        return "No documents found."
+        
+   
+    return "\n\n".join(
+        f"Filename: {doc.get('document_name', 'Unknown').replace('.md', '')}\n"
+        f"Filepath: {doc.get('filepath', 'Unknown')}\n"
+        f"Content: {doc['content']}"
+        for doc in results
+    )
+
+
+async def bot_answer(user_prompt: str):
+    try:
+        response = await rag_agent.run(user_prompt)
+        return response.output
+    except Exception as e:
+        return RagResponse(
+            filename="Error",
+            filepath="Error",
+            answer=f"Något gick fel i Agenten: {str(e)}"
+        )
