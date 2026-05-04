0be6a02fe497fd25eeecf2b022f470f90b3aac4b
Josefin Lesley <josefin3647@gmail.com>
2026-05-04 22:54:54 +0200

    Added a docstring to register_system_prompt and some minor spelling improvements

--- prompt_engineering/prompt_loader.py
+++ prompt_engineering/prompt_loader.py
@@ -2,14 +2,14 @@
 from backend.constants import PROMPTS_PATH
 from mlflow.genai import register_prompt
 
-def register_system_prompt(**kwargs):
-    for filepath in PROMPTS_PATH.rglob("*md"):
-        with open(filepath) as file:
+def register_system_prompts(**kwargs):
+    """Registers all .md prompt templates in PROMPTS_PATH in MLflow."""
+    for filepath in PROMPTS_PATH.rglob("*.md"):
+        with open(filepath, encoding="utf-8") as file:
             filename = filepath.stem
             prompt = file.read()
-
         
         register_prompt(name=filename, template=prompt, **kwargs)
 
 if __name__ == "__main__":
-    register_system_prompt() 
+    register_system_prompts() 
