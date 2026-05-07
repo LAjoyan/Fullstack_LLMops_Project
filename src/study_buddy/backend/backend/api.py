592d2c4b43ae256a7bf8f9e4f67138090ca80029
LAjoyan <ajoyan.lilit@gmail.com>
2026-05-07 17:23:04 +0200

    fix: remove typo and load environment variables with dotenv

--- src/study_buddy/backend/backend/api.py
+++ src/study_buddy/backend/backend/api.py
@@ -1,13 +1,17 @@
+from dotenv import load_dotenv  # this must be at the top otherwise won't work
+
+load_dotenv()
 from fastapi import FastAPI
 from backend.data_models import Prompt, RagResponse
 from backend.agents import bot_answer
 
-
 app = FastAPI()
+
 
 @app.get("/")
 async def status():
     return {"status": "it works"}
+
 
 @app.post("/rag/query")
 async def query_documentation(query: Prompt) -> RagResponse:
